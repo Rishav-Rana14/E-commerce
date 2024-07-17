e23ca9dc7bf7a5a9801b1eb37cd54b47590486ac
Rishav Rana <rishavrana@externlabs.com>
2024-07-17 12:12:24 +0530

    make search bar

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,34 +1,52 @@
-import React from 'react'
-import logo from '../../Assests/Untitled.jpeg'
-import { Link } from 'react-router-dom'
-import CountryDropDown from '../CountryDropdown';
+import React from "react";
+import logo from "../../Assests/Untitled.jpeg";
+import { Link } from "react-router-dom";
+import CountryDropDown from "../CountryDropdown";
+import { IoSearch } from "react-icons/io5";
+import Button from "@mui/material/Button";
 
 const Header = () => {
   return (
     <>
-    <div className='headerWrapper'>
-      <div className='top-stripe bg-purple'>
-        <div className='container'>
-          <p className='mb-0 mt-0 text-center'>Due to the <b>COVID-19</b> epidemic, orders may be processed with a 
-          slight delay</p>
-        </div>
-      </div>
-       
-      <div className='header'>
-        <div className='container'>
-          <div className='row'>
-            <div className='logoWrapper d-flex align-items-center col-sm-2'>
-              <Link to={'/'}> <img src={logo} alt='Logo'/></Link>
-            </div>
-            <div className='col-sm-10 d-flex align-items-center part2'>
-              <CountryDropDown/>
-            </div>
+      <div className="headerWrapper">
+        <div className="top-stripe bg-purple">
+          <div className="container">
+            <p className="mb-0 mt-0 text-center">
+              Due to the <b>COVID-19</b> epidemic, orders may be processed with
+              a slight delay
+            </p>
           </div>
         </div>
-      </div>
-    </div>
-    </>
-  )
-}
 
-export default Header
+        <header className="header">
+          <div className="container">
+            <div className="row">
+              <div className="logoWrapper d-flex align-items-center col-sm-2">
+                <Link to={"/"}>
+                  {" "}
+                  <img src={logo} alt="Logo" />
+                </Link>
+              </div>
+              <div className="col-sm-10 d-flex align-items-center part2">
+                <CountryDropDown />
+
+                {/* { Header Search Start} */}
+
+                <div className="headerSearch ml-3 mr-3">
+                  <input type="text" />
+                  <Button>
+                    <IoSearch />
+                  </Button>
+                </div>
+
+                {/* { Header Search Start} */}
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    </>
+  );
+};
+
+export default Header;
